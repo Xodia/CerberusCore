2ec9684aba66483d144389ea3b53ca681601c10e
Morgan Collino <mcollino@stashinvest.com>
2020-04-10 16:18:57 -0400

    Fix tests

--- Tests/CerberusCoreTests/CerberusCoreTests.swift
+++ Tests/CerberusCoreTests/CerberusCoreTests.swift
@@ -6,7 +6,6 @@ final class CerberusCoreTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CerberusCore().text, "Hello, World!")
     }
 
     static var allTests = [
